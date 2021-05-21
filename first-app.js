c6383cc38e96ddc50be7fd158237e1c964119cc3
balwinderOpulence <itsupport5@opulence.com>
2021-05-21 17:21:18 -0400

    complete debug section

--- first-app.js
+++ first-app.js
@@ -1,2 +1,11 @@
 const fs = require('fs');
-fs.writeFileSync('hello.txt', 'Hello from node js again 123');
+fs.writeFileSync('test.txt', 'Hello from node js again 123');
+
+
+//core modules of nodejs some of them  
+//http 
+//https
+//fs
+//path
+//os
+
